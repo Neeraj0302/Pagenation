18b8658702a2f680388d7ec338f165479c0a42d2
Neeraj <vneeraj521@gmail.com>
2022-09-28 13:49:07 +0530

    CRUD MODULE

--- MyModule/Controller/Index/Index.php
+++ MyModule/Controller/Index/Index.php
@@ -1,24 +1,46 @@
 <?php
 
-
 namespace Dolphin\MyModule\Controller\Index;
 
-use Magento\Framework\DataObject;
 use Magento\Framework\App\Action\Action;
 
 class Index extends Action
 {
+    protected $_resultPageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        $blogData = new DataObject(['blog_id' => '1', 'blog_title' => 'Blog Title']);
-        $this->_eventManager->dispatch('dolphin_MyModule_blog',
-            [
-                'blog' => $blogData
-            ]
-        );
-        echo "Dolphin MyModule Event";exit;
+        $resultPage = $this->_resultPageFactory->create();
+        return $resultPage;
     }
 }
+
+// namespace Dolphin\MyModule\Controller\Index;
+
+// use Magento\Framework\DataObject;
+// use Magento\Framework\App\Action\Action;
+
+// class Index extends Action
+// {
+//     public function execute()
+//     {
+//         $blogData = new DataObject(['blog_id' => '1', 'blog_title' => 'Blog Title']);
+//         $this->_eventManager->dispatch('dolphin_MyModule_blog',
+//             [
+//                 'blog' => $blogData
+//             ]
+//         );
+//         echo "Dolphin MyModule Event";exit;
+//     }
+// }
 
 
 // namespace Dolphin\MyModule\Controller\Index;
